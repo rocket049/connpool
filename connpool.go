a260a466dff0ee8a75c3aaa3650584c1ef6c9fe5
rocket049 <fuhuizn@163.com>
2019-06-14 13:27:39 +0800

    2019年 06月 14日 星期五 13:27:39 CST

--- connpool.go
+++ connpool.go
@@ -1,88 +1,88 @@
 package connpool
 
-import(
-	"net"
+import (
 	"container/list"
-	"time"
+	"net"
 	"sync"
+	"time"
 )
 
-type Conn struct{
-	conn net.Conn
-	deadline  time.Time
-	timeout int
+type Conn struct {
+	conn     net.Conn
+	deadline time.Time
+	timeout  int
 }
 
-func (s *Conn)Read(p []byte)(int,error){
-	n,err := s.conn.Read(p)
-	if err!=nil{
-		return n,err
+func (s *Conn) Read(p []byte) (int, error) {
+	n, err := s.conn.Read(p)
+	if err != nil {
+		return n, err
 	}
 	s.setDeadline()
-	return n,nil
+	return n, nil
 }
 
-func (s *Conn)Write(p []byte)(int,error){
-	n,err := s.conn.Write(p)
-	if err!=nil {
-		return n,err
+func (s *Conn) Write(p []byte) (int, error) {
+	n, err := s.conn.Write(p)
+	if err != nil {
+		return n, err
 	}
 	s.setDeadline()
-	return n,nil
+	return n, nil
 }
 
-func (s *Conn)Close()error {
+func (s *Conn) Close() error {
 	err := s.conn.Close()
 	s.deadline = time.Now()
 	return err
 }
 
-func (s *Conn)setDeadline() {
+func (s *Conn) setDeadline() {
 	s.deadline = time.Now().Add(time.Duration(s.timeout) * time.Second)
 	s.conn.SetDeadline(s.deadline)
 }
 
-func (s *Conn)Timeout() bool {
-	return time.Now().Unix()>=s.deadline.Unix()
+func (s *Conn) Timeout() bool {
+	return time.Now().Unix() >= s.deadline.Unix()
 }
 
 type Pool struct {
-	lock *sync.Mutex
-	cond *sync.Cond
-	max int
-	timeout int
-	newFunc func()(net.Conn,error)
+	lock        *sync.Mutex
+	cond        *sync.Cond
+	max         int
+	timeout     int
+	newFunc     func() (net.Conn, error)
 	connections *list.List
 }
 
-func NewPool(max,timeout int,factory func()(net.Conn,error) ) *Pool {
+func NewPool(max, timeout int, factory func() (net.Conn, error)) *Pool {
 	l := new(sync.Mutex)
-	return &Pool{lock:new(sync.Mutex),cond:sync.NewCond(l),max:max,timeout:timeout,newFunc:factory,
-			connections:list.New()}
+	return &Pool{lock: new(sync.Mutex), cond: sync.NewCond(l), max: max, timeout: timeout, newFunc: factory,
+		connections: list.New()}
 }
 
-func (s *Pool)newConn()(*Conn,error){
-	conn,err := s.newFunc()
-	if err!=nil{
-		return nil,err
+func (s *Pool) newConn() (*Conn, error) {
+	conn, err := s.newFunc()
+	if err != nil {
+		return nil, err
 	}
-	
-	deadline := time.Now().Add(time.Duration(s.timeout)*time.Second)
+
+	deadline := time.Now().Add(time.Duration(s.timeout) * time.Second)
 	conn.SetDeadline(deadline)
-	return &Conn{conn:conn,deadline:deadline,timeout:s.timeout}, nil
+	return &Conn{conn: conn, deadline: deadline, timeout: s.timeout}, nil
 }
 
-func (s *Pool)Get() (*Conn,error){
+func (s *Pool) Get() (*Conn, error) {
 	var res *Conn
 	var wait bool
 	s.lock.Lock()
-	if s.max>0 {
+	if s.max > 0 {
 		s.max--
-	}else{
+	} else {
 		wait = true
 	}
 	s.lock.Unlock()
-	
+
 	if wait {
 		s.cond.L.Lock()
 		s.cond.Wait()
@@ -90,25 +90,26 @@ func (s *Pool)Get() (*Conn,error){
 	}
 	s.lock.Lock()
 	defer s.lock.Unlock()
-	if s.connections.Len()>0{
+	if s.connections.Len() > 0 {
 		e := s.connections.Front()
 		res = e.Value.(*Conn)
 		s.connections.Remove(e)
-	}else{
+	} else {
 		return s.newConn()
 	}
 	if res.Timeout() {
+		res.Close()
 		return s.newConn()
-	}else{
-		return res,nil
+	} else {
+		return res, nil
 	}
 }
 
-func (s *Pool)Put(conn1 *Conn){
+func (s *Pool) Put(conn1 *Conn) {
 	var wait bool
 	s.lock.Lock()
 	s.connections.PushBack(conn1)
-	if s.max==0{
+	if s.max == 0 {
 		wait = true
 	}
 	s.lock.Unlock()
@@ -117,9 +118,9 @@ func (s *Pool)Put(conn1 *Conn){
 	}
 }
 
-func (s *Pool)Close(){
+func (s *Pool) Close() {
 	e := s.connections.Front()
-	if e.Value!=nil {
+	if e.Value != nil {
 		conn := e.Value.(*Conn)
 		conn.Close()
 		e = e.Next()
