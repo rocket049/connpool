bda9950f094bbf90737c6e9663b1bfb1a57bca98
rocket049 <fuhuizn@163.com>
2019-06-14 10:07:29 +0800

    2019年 06月 14日 星期五 10:07:29 CST

---
+++ connpool.go
@@ -0,0 +1,128 @@
+package connpool
+
+import(
+	"net"
+	"container/list"
+	"time"
+	"sync"
+)
+
+type Conn struct{
+	conn net.Conn
+	deadline  time.Time
+	timeout int
+}
+
+func (s *Conn)Read(p []byte)(int,error){
+	n,err := s.conn.Read(p)
+	if err!=nil{
+		return n,err
+	}
+	s.setDeadline()
+	return n,nil
+}
+
+func (s *Conn)Write(p []byte)(int,error){
+	n,err := s.conn.Write(p)
+	if err!=nil {
+		return n,err
+	}
+	s.setDeadline()
+	return n,nil
+}
+
+func (s *Conn)Close()error {
+	err := s.conn.Close()
+	s.deadline = time.Now()
+	return err
+}
+
+func (s *Conn)setDeadline() {
+	s.deadline = time.Now().Add(time.Duration(s.timeout) * time.Second)
+	s.conn.SetDeadline(s.deadline)
+}
+
+func (s *Conn)Timeout() bool {
+	return time.Now().Unix()>=s.deadline.Unix()
+}
+
+type Pool struct {
+	lock *sync.Mutex
+	cond *sync.Cond
+	max int
+	timeout int
+	newFunc func()(net.Conn,error)
+	connections *list.List
+}
+
+func NewPool(max,timeout int,factory func()(net.Conn,error) ) *Pool {
+	l := new(sync.Mutex)
+	return &Pool{lock:new(sync.Mutex),cond:sync.NewCond(l),max:max,timeout:timeout,newFunc:factory,
+			connections:list.New()}
+}
+
+func (s *Pool)newConn()(*Conn,error){
+	conn,err := s.newFunc()
+	if err!=nil{
+		return nil,err
+	}
+	
+	deadline := time.Now().Add(time.Duration(s.timeout)*time.Second)
+	conn.SetDeadline(deadline)
+	return &Conn{conn:conn,deadline:deadline,timeout:s.timeout}, nil
+}
+
+func (s *Pool)Get() (*Conn,error){
+	var res *Conn
+	var wait bool
+	s.lock.Lock()
+	if s.max>0 {
+		s.max--
+	}else{
+		wait = true
+	}
+	s.lock.Unlock()
+	
+	if wait {
+		s.cond.L.Lock()
+		s.cond.Wait()
+		s.cond.L.Unlock()
+	}
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	if s.connections.Len()>0{
+		e := s.connections.Front()
+		res = e.Value.(*Conn)
+		s.connections.Remove(e)
+	}else{
+		return s.newConn()
+	}
+	if res.Timeout() {
+		return s.newConn()
+	}else{
+		return res,nil
+	}
+}
+
+func (s *Pool)Put(conn1 *Conn){
+	var wait bool
+	s.lock.Lock()
+	s.connections.PushBack(conn1)
+	if s.max==0{
+		wait = true
+	}
+	s.lock.Unlock()
+	if wait {
+		s.cond.Signal()
+	}
+}
+
+func (s *Pool)Close(){
+	e := s.connections.Front()
+	if e.Value!=nil {
+		conn := e.Value.(*Conn)
+		conn.Close()
+		e = e.Next()
+	}
+	s.connections.Init()
+}
---
+++ connpool_test.go
@@ -0,0 +1,82 @@
+package connpool
+
+import(
+	"net"
+	"sync"
+	"time"
+	"testing"
+	"fmt"
+	"os"
+	"log"
+	"bytes"
+)
+
+func factory() (net.Conn,error) {
+	return net.Dial("tcp","127.0.0.1:7060")
+}
+
+func TestPool(t *testing.T) {
+	pool1 := NewPool(10, 30 ,factory)
+	var err1 error
+	var wg sync.WaitGroup
+	for i:=0;i<50;i++ {
+		wg.Add(1)
+		go func(n int){
+			conn ,err := pool1.Get()
+			if err!=nil {
+				err1 = err
+			}
+			//t.Log(n,err, time.Now().Format("15:04:05"))
+			time.Sleep( 5*time.Second )
+			msg := []byte( fmt.Sprintf("msg%d\n",n) )
+			buf := make([]byte,len(msg))
+			_,err = conn.Write( msg )
+			if err!=nil{
+				t.Log(err)
+			}
+			n1,err := conn.Read( buf )
+			if err!=nil{
+				t.Log(err)
+			}else if bytes.Compare( msg[:n1] , buf[:n1] )!=0 {
+				t.Log("Fail",msg[:n1],buf[:n1])
+			}
+			pool1.Put(conn)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	if err1!=nil {
+		t.Fatal(err1)
+	}
+}
+
+func TestMain(m *testing.M) {
+	l, e := net.Listen("tcp", "127.0.0.1:7060")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	defer l.Close()
+	go echoServer(l)
+	os.Exit(m.Run())
+}
+
+func echoServer(l net.Listener) {
+	for i:=0;i<50;i++ {
+		conn,err := l.Accept()
+		if err!=nil {
+			break
+		}
+		go func(conn1 net.Conn) {
+			var buf [100]byte
+			for {
+				n,err:=conn1.Read(buf[:])
+				if err==nil{
+					conn1.Write(buf[:n])
+				}else{
+					break
+				}
+			}
+			conn1.Close()
+		}(conn)
+	}
+}
